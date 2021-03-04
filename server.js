3c07a6680ac42b372124d5ace7d5b938d01992f1
jansferr <sferrazzajane@gmail.com>
2021-03-04 15:09:33 -0500

    changed

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const mongoose = require("mongoose");
+// const morgan = require("morgan");
+const logger = require("morgan");
+const PORT = process.env.PORT || 5000;
+
+const app = express();
+
+app.listen(PORT, () => {
+    console.log(`You're up and running on port ${PORT}`);
+})
+//connect to mongoose
+mongoose.connect(process.env.MONGO_URI || "mongodb://localhost/workout", {
+  useNewUrlParser: true,
+  useFindAndModify: false
+});
+
+//login request middleware
+app.use(logger("dev"));
+
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static("public"));
+
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+  });
+  
+
+
